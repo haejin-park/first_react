cafbf3ff5423f4c527a249b2f95fbbc15131b1bf
haejin <luckyhaejin1126@gmail.com>
2024-02-07 12:32:05 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import {useState} from 'react';
 import './App.css';
-import Box from "./component/Box";
 
 function App() {
+  let counter = 0;
+  const [counter2, setCounter2] = useState(0);
+  const increase = () => {
+    counter = counter + 1;
+    setCounter2(counter2+1)
+    console.log('counter', counter, "counter2", counter2);
+  };
+
+  /*
+  순서
+  유저가 버튼 클릭
+  counter+1해서 1이됨
+  setState함수 호출
+  console.log 실행됨
+
+  변수값은 1로 보이고 state 값은 아직 안변했기 때문에 그 전에 값이 보인다
+  함수 끝
+  app 다시 re render
+  let counter = 0을 거치면서 counter값은 다시 0으로 초기화가 된다
+  update된 state값이 보인다 
+*/
+  
+
   return (
     <div>
-      <Box name="제니" num={1}/>
-      <Box name="리사" num={2}/>
-      <Box name="지수" num={3}/>
+      <div>{counter}</div>
+      <div>state: {counter2}</div>
+      <button onClick={increase}>클릭!</button>
     </div>
   );
 }
