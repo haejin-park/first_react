42458a23836ba0a9907f81c0488cb549d554e682
haejin <luckyhaejin1126@gmail.com>
2024-02-07 12:24:05 +0900

    props

---
+++ src/component/Box.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Box = (props) => {
+    console.log("props: ", props);
+  return (
+    <div>
+        <div className="box">
+            Box{props.num}
+            <p>{props.name}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Box
