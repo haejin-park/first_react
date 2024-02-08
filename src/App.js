957df8eaadd62905a5dd52d3fa6409ed5b96ec01
haejin <luckyhaejin1126@gmail.com>
2024-02-08 21:58:07 +0900

    함수형 컴포넌트 라이프사이클

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
   let counter = 0;
   const [counter2, setCounter2] = useState(0);
+  const [value, setValue] = useState(0);
   const increase = () => {
     counter = counter + 1;
-    setCounter2(counter2+1)
-    console.log('counter', counter, "counter2", counter2);
+    setCounter2(counter2+1);
+    setValue(value+2);
+    console.log('counter', counter, "counter2", counter2, "value", value);
   };
 
   /*
@@ -23,10 +25,22 @@ function App() {
   let counter = 0을 거치면서 counter값은 다시 0으로 초기화가 된다
   update된 state값이 보인다 
 */
-  
+  useEffect(() => {
+    console.log("useEffect1 Fire");
+  },[]);
+
+  useEffect(() => {
+    console.log("useEffect2 Fire", counter2);
+  },[counter2]);
+
+
+  useEffect(() => {
+    console.log("useEffect3 다른내용", value);
+  },[value]);
 
   return (
     <div>
+      {console.log("render")}
       <div>{counter}</div>
       <div>state: {counter2}</div>
       <button onClick={increase}>클릭!</button>
