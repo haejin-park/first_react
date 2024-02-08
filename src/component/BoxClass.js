ca1b6b6a0afb2f3c23b7668e081de67bd9df4856
haejin <luckyhaejin1126@gmail.com>
2024-02-08 21:30:07 +0900

    클래스 컴포넌트 라이프사이클

--- src/component/BoxClass.js
+++ src/component/BoxClass.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 
 export default class BoxClass extends Component {
-
+componentWillUnmount() {
+  console.log("bye");
+}
   render() {
     return <div>Box{this.props.num}</div>;
   }
